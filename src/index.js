4d7b9de97df29ccc943e5fdcf0b9dbef3be58793
YasenYanev <124697579+YasenYanev@users.noreply.github.com.>
2024-10-19 20:22:02 +0300

    feat: Ship class created

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import "./styles/main.css";
+
+class Ship {
+  constructor(length) {
+    this.length = length;
+    this.hits = 0;
+    this.hasSunk = false;
+  }
+
+  hit() {
+    this.hits += 1;
+  }
+
+  isSunk() {
+    if (this.hits === this.length) this.hasSunk = true;
+  }
+}
+
+export { Ship };
--- test/.test.js
+++ test/.test.js
@@ -1,1 +1,22 @@
-// import {some code} from './index';
+import { Ship } from "../src/index";
+
+let ship;
+
+beforeEach(() => {
+  ship = new Ship(3);
+});
+
+describe("Ship class test", () => {
+  test("Test ship hits", () => {
+    ship.hit();
+    expect(ship.hits).toEqual(1);
+  });
+
+  test("Test ship sunk method", () => {
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    ship.isSunk();
+    expect(ship.hasSunk).toBe(true);
+  });
+});
